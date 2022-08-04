831417915e9bb5581b2305f61bfeffb1af2aa275
MrLoGGo <logo123444445@gmail.com>
2022-08-04 17:09:04 +0300

    д

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        //I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000;
+        String[] num_roman = {"I", "V", "X", "L", "C", "D", "M"};
+        System.out.print("enter num: ");
+        Scanner str = new Scanner(System.in);
+        String num = str.next();
+        String roman_num = "";
+        for (int i = 0; i < num.length();i++){
+            if (num.length() - i == 1){
+                roman_num += count_roman_digit(num.charAt(i) - '0', "I", "V", "X");
+            }
+            else if (num.length() - i == 2) {
+                roman_num += count_roman_digit(num.charAt(i) - '0', "X", "L" , "C");
+                /*System.out.println(roman_num + '1');*/
+            }
+            else if (num.length() - i == 3 ) {
+                roman_num += count_roman_digit(num.charAt(i) - '0', "C", "D" , "M");
+            }
+            else if (num.length() - i == 4 ) {
+                roman_num += "M";
+            }
+            else return;
+        }
+        System.out.println(roman_num);
+    }
+    public static String count_roman_digit(int num, String first_roman_digit, String second_roman_digit, String third_roman_digit){
+        String end_roman_digit = "";
+        if(num < 4){
+            for(int i = 0; i < num; i++){
+                end_roman_digit += first_roman_digit;
+                /*System.out.println("0");*/
+            }
+        } else if (num == 4) {
+            end_roman_digit = first_roman_digit + second_roman_digit;
+        }else if(num == 5){
+            end_roman_digit = second_roman_digit;
+        } else if (num > 5 && num < 9) {
+            end_roman_digit = second_roman_digit;
+            for(int i = 0; i < num-5; i++){
+                end_roman_digit += first_roman_digit;
+            }
+        } else if (num == 9) {
+            end_roman_digit = first_roman_digit + third_roman_digit;
+        }/*
+        System.out.print(end_roman_digit + '2' + first_roman_digit);*/
+        return end_roman_digit;
+    }
+
+}
